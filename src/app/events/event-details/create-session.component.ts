4d45233cd6b974f65761d5d570706eac856c6bd8
Enrique <enrique.pastrana@gmail.com>
2022-11-01 11:48:12 +0100

    Reusing Components with Ctonetn projection

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -35,7 +35,7 @@ export class CreateSessionComponent implements OnInit {
         this.presenter = new FormControl('', Validators.required);
         this.duration = new FormControl('', Validators.required);
         this.level = new FormControl('', Validators.required);
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])]);
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo'])]);
 
         this.newSessionForm = new FormGroup({
             name: this.name,
